2eb83485e3b1a8afcb98eeed0f5bfc3fe2e451b4
Mario Garcia <mario.ggar@gmail.com>
2022-09-15 00:24:27 +0200

    fix(logs): uncomment logs in config.js

--- packages/ghops-core/config.js
+++ packages/ghops-core/config.js
@@ -1,5 +1,5 @@
 import file from "fs";
-//import { logger } from "./logger";
+import { logger } from "./logger.js";
 
 const ENCODING = "utf-8";
 const DEFAULT_CONFIG_PATH = "config.json";
@@ -7,15 +7,15 @@ const DEFAULT_CONFIG_PATH = "config.json";
 function parseConfig() {
   let configuration = {};
   let configPath = process.env.CONFIG_PATH || DEFAULT_CONFIG_PATH;
-  //let actionLogger = logger.child({ action: "parse-config" });
+  let actionLogger = logger.child({ action: "parse-config" });
 
   try {
-    //actionLogger.debug({ step: "reading-file" });
+    actionLogger.debug({ step: "reading-file" });
     configuration = JSON.parse(
       file.readFileSync(configPath, { encoding: ENCODING })
     );
   } catch (err) {
-    //actionLogger.error({ step: "error", cause: err.message });
+    actionLogger.error({ step: "error", cause: err.message });
   }
 
   return configuration;
