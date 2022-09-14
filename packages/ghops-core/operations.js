35e1f2353eb3e947f25ebf6f073093a7be13c680
Mario Garcia <mario.ggar@gmail.com>
2022-09-15 00:41:16 +0200

    chores(docs): adding some explanation to Op

--- packages/ghops-core/operations.js
+++ packages/ghops-core/operations.js
@@ -1,4 +1,26 @@
+/**
+ * Represents an operation. And operation has a plan and
+ * the operation can eventually execute that plan
+ */
 class Op {
+  /**
+   * This method should be executed to know which actions
+   * will be finally executed. Some actions could have
+   * been already taken and we may not want to execute them
+   * again
+   *
+   * Besides the user could only want to do a dry-run and
+   * see what would have been the outcome in case it may
+   * have been run
+   */
+  plan() {
+    throw new Error("operation not supported!");
+  }
+
+  /**
+   * This method applies all changes included in the operation
+   * plan
+   */
   execute() {
     throw new Error("operation not supported!");
   }
